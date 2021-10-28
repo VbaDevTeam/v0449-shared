cd41343915bb9ee69ef5bbd380d89cef2a787a83
vroby67 <r.visca@vbanet.eu>
2021-10-28 20:38:14 +0200

    Integrazioni per 0449 (comunicazione)

--- v0449-shared/Classi Comuni.cs
+++ v0449-shared/Classi Comuni.cs
@@ -562,67 +562,110 @@ namespace v0449_shared
 #if v0449
   public class ComRt2Plc
   {
-    public UInt16[] diFs = new UInt16[4];
-    public UInt16[] diFv = new UInt16[4];
-    public UInt16[] doFs = new UInt16[4];
-    public UInt16[] doFv = new UInt16[4];
-    public UInt16[] cmdReq = new UInt16[1];
+    public ComRt2Plc()
+    {
+      diFs = new UInt16[3];
+      diFv = new UInt16[3];
+      doFs = new UInt16[3];
+      doFv = new UInt16[3];
+      c1 = new();
+//      c2 = new();
+    }
+    public UInt16[] diFs { get; set; }
+    public UInt16[] diFv { get; set; }
+    public UInt16[] doFs { get; set; }
+    public UInt16[] doFv { get; set; }
+    public Int16 selCircB { get; set; }
+    public Int16 wDogSrv { get; set; }
+    public Int16 wDogCli { get; set; }
 
-    public Int16 spTVasca_d;
-    public Int16 spTCeMan_d;
-    public Int16 spTFlMan_d;
-    public Int16 spPLavMan_d;
-    public Int16 spPRipMan_d;
-    public Int16 spTRampaPMan_c;
-    public Int16 spQFlMan_c;
-    public Int16 spTCarico_c;
-    public Int16 spTScarco_c;
-    public Int16 spOverpPerc;
-    public int cmdReqCli;
-    public int ricNoToSend;
-
+    public C2Plc c1 { get; set; }
+    //public C2Plc c2 { get; set; }
   }
-  public class ComRt2Hmi
+  public class C2Plc
   {
-    public UInt16 di0;
-    public UInt16 di1;
-    public UInt16 di2;
-    public UInt16 di3;
-    public UInt16 do0;
-    public UInt16 do1;
-    public UInt16 do2;
-    public UInt16 do3;
-    public UInt16 cmdStAut;
-    public UInt16 cmdStMan;
-    public UInt16 cmdSt;
-    public Int16[] AI = new short[16];
-    public UInt16[] alarms = new ushort[5];
-    public Int16 ptrPh1Frig;
-    public Int16 ptrPhRiscCe;
-    public Int16 ptrPhRaffrCe;
-    public Int16 ptrPhCompCe;
-    public Int16 ptrPhTest;
-    public Int16 ptrPhPulsa;
-    public Int16 idNo;
-    public Int16 flVari;
-    public Int16 ptrStep;
-    public Int16 cntRip;
-    public Int16 spTCeAut_d;
-    public Int16 spTFlAut_d;
-    public Int16 spPresFl_d;
-    public Int16 pidPwRisCe_d;
-    public Int16 pidPwRafCe_d;
-    public Int16 pidPwRisFl_d;
-    public Int16 pidPwRafFl_d;
-    public Int16 cntTmStep;
-    public int cntCicStep;
-    public int cntTmTest;
-    public int cntCicTest;
-    public DateTime tmSync;
-    public Int16 tappo;
-    public int cmdRespSrv;
-    public bool recDataOn;
+    public C2Plc()
+    {
+      cmdReq = new UInt16[1];
+    }
+    public UInt16[] cmdReq { get; set; }
+
+    public Int16 spTVasca_d { get; set; }
+    public Int16 spTCeMan_d { get; set; }
+    public Int16 spTFlMan_d { get; set; }
+    public Int16 spPLavMan_d { get; set; }
+    public Int16 spPRipMan_d { get; set; }
+    public Int16 spTRampaPMan_c { get; set; }
+    public Int16 spQFlMan_c { get; set; }
+    public Int16 spTCarico_c { get; set; }
+    public Int16 spTScarco_c { get; set; }
+    public Int16 spOverpPerc { get; set; }
+    public Int16 comStatus { get; set; }
+    public int cmdReqCli { get; set; }
+    public int ricNoToSend { get; set; }
+
   }
+
+public class ComRt2Hmi
+{
+  public ComRt2Hmi()
+  {
+    AI = new short[18];
+    alarms = new ushort[5];
+    c1 = new C2Hmi();
+    c2 = new C2Hmi();
+  }
+  public UInt16 di0 { get; set; }
+  public UInt16 di1 { get; set; }//;
+  public UInt16 di2 { get; set; }//;
+  public UInt16 di3 { get; set; }//;
+  public UInt16 do0 { get; set; }//;
+  public UInt16 do1 { get; set; }//;
+  public UInt16 do2 { get; set; }//;
+  public UInt16 do3 { get; set; }//;
+  public Int16[] AI { get; set; }//;
+  public UInt16[] alarms { get; set; }//;
+  public DateTime tmSync { get; set; }//;
+  public int cmdRespSrv { get; set; }//;
+  public bool recDataOn { get; set; }//;
+  public C2Hmi c1 { get; set; }
+  public C2Hmi c2 { get; set; }
+}
+public class C2Hmi
+{
+  public C2Hmi()
+  {
+  }
+  public Int16 comStatus { get; set; }//;
+  public Int16 runStatus { get; set; }//;
+  public Int16 spTCeMan_d { get; set; }//;
+  public UInt16 cmdStAut { get; set; }//;
+  public UInt16 cmdStMan { get; set; }//;
+  public UInt16 cmdSt { get; set; }//;
+  public Int16 flVari { get; set; }//;
+  public Int16 ptrPhTest { get; set; }//;
+  public Int16 ptrPhPulsa { get; set; }//;
+  public Int16 ptrPh1Frig { get; set; }//;
+  public Int16 ptrPhRiscCe { get; set; }//;
+  public Int16 ptrPhRaffrCe { get; set; }//;
+  public Int16 ptrPhCompCe { get; set; }//;
+
+  public Int16 idNo { get; set; }//;
+  public Int16 ptrStep { get; set; }//;
+  public Int16 cntRip { get; set; }//;
+  public Int16 spTCeAut_d { get; set; }//;
+  public Int16 spTFlAut_d { get; set; }//;
+  public Int16 spPresFl_d { get; set; }//;
+  public Int16 pidPwRisFl_d { get; set; }//;
+  public Int16 pidPwRafFl_d { get; set; }//;
+  public Int16 cntTmStep { get; set; }//;
+  public int cntCicStep { get; set; }//;
+  public int cntTmTest { get; set; }//;
+  public int cntCicTest { get; set; }//;
+  public int cmdRespSrv { get; set; }//;
+  public bool recDataOn { get; set; }//;
+} 
+
 #elif v0470
 
   public class ComRt2Plc
